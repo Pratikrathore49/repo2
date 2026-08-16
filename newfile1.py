489790339982f45e9bc8d86498d7d372bcc232bc
Pratik Rathore <jipratik49@gmail.com>
2026-08-16 17:24:43 +0530

    Revert "Merge branch 'try/branch1' of https://github.com/Pratikrathore49/repo2 into try/branch1"
    This reverts commit fc7bc0060776e5f09d9cdb964890beb2909e607d, reversing
    changes made to 8988eb9f1e8031351c33c483618902a3e67e99d8.

--- newfile1.py
+++ newfile1.py
@@ -1,10 +1,2 @@
 # Hello i am on try/branch1
 #hello i am change my code form try/brach2
-#commit1
-#commit2
-#commit3
-#today's commit 
-#today's commit3
-#today's commit2
-
-# make perfack git revert -m 1 commit
