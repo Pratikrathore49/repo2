e03c4a91b09b6b71bd739e2d0f55e74bc1138cae
Pratik Rathore <jipratik49@gmail.com>
2026-08-16 18:40:39 +0530

    Revert "#today's commit -check pratik"
    This reverts commit ac818774441a49307fbca93d22a7756ec22d0a60.

--- newfile1.py
+++ newfile1.py
@@ -1,2 +1,8 @@
 # Hello i am on try/branch1
 #hello i am change my code form try/brach2
+<<<<<<< HEAD
+=======
+#commit1
+#commit2
+#commit3
+>>>>>>> parent of ac81877 (#today's commit -check pratik)
